2d9045d7cd5ed5a828cd61834f4471c924008a44
circle1214 <163132022+circle1214@users.noreply.github.com>
2025-03-21 15:07:02 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "quan",
+    name: "Quan",
     image: "notion-avatar-1742295171248.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Engineer",
     bio: "Share my thoughts.",
